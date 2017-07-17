56670afe26fabd3395ebf1936990f4252c6c07b3
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-16 21:04:54 -0400

    Finished UI, and parsed collection view. Enabled dequeue reusable cell

---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Justin Lindsay on 7/16/17.
+//  Copyright © 2017 Justin Lindsay. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+        
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
